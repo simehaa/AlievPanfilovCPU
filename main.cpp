9474de6a833e9474bd7f82bda1d60b50fbdbe1be
simehaa <simen.haapnes@gmail.com>
2022-04-21 10:16:13 +0200

    fix indentations: only tabs, no spaces

--- main.cpp
+++ main.cpp
@@ -3,46 +3,46 @@
 #include <fstream>
 
 unsigned index(unsigned x, unsigned y, unsigned z, unsigned width, unsigned depth) {
-	// 1D index corresponding to a flattened 3D variable. 
+  // 1D index corresponding to a flattened 3D variable. 
   return z + (y*depth) + (x*width*depth);
 }
 
 int main (int argc, char** argv) {
-	// Constants in the PDE computation
-	const float my1 = 0.07;
-	const float my2 = 0.3;
-	const float delta = 5.0e-5;
-	const float epsilon = 0.01;
-	const float a = 0.1;
-	const float b = 0.1;
-	const float k = 8.0;
-	const float dx = 0.000143;
-	const float dt = 0.0001;
-	
-	// Mesh dimensions and number of iterations
-	const std::size_t num_iterations = 20000;
-	const std::size_t h = 50;
-	const std::size_t w = 50;
-	const std::size_t d = 50;
+  // Constants in the PDE computation
+  const float my1 = 0.07;
+  const float my2 = 0.3;
+  const float delta = 5.0e-5;
+  const float epsilon = 0.01;
+  const float a = 0.1;
+  const float b = 0.1;
+  const float k = 8.0;
+  const float dx = 0.000143;
+  const float dt = 0.0001;
 
-	// Padded mesh dimensions and respective volumes
-	const std::size_t hp = h + 2;
-	const std::size_t wp = w + 2;
-	const std::size_t dp = d + 2;
-	const std::size_t volume = h*w*d;
-	const std::size_t padded_volume = hp*wp*dp;
+  // Mesh dimensions and number of iterations
+  const std::size_t num_iterations = 20000;
+  const std::size_t h = 50;
+  const std::size_t w = 50;
+  const std::size_t d = 50;
 
-	// Mesh variables
+  // Padded mesh dimensions and respective volumes
+  const std::size_t hp = h + 2;
+  const std::size_t wp = w + 2;
+  const std::size_t dp = d + 2;
+  const std::size_t volume = h*w*d;
+  const std::size_t padded_volume = hp*wp*dp;
+
+  // Mesh variables
   std::vector<float> e_mesh(padded_volume);
-	std::vector<float> e_temp(volume);
+  std::vector<float> e_temp(volume);
   std::vector<float> r_mesh(volume);
   float e_center;
-	float r_center;
+  float r_center;
 
-	// Other variables
+  // Other variables
   const float d_dx2 = delta/(dx*dx);
   std::size_t write_counter = 0; // Number of files that have been written
-	std::size_t write_frequency = 150; // How frequent (number of iterations) to write
+  std::size_t write_frequency = 150; // How frequent (number of iterations) to write
   std::size_t slice = d / 2; // Which slice index (along depth) to write
 
   // Initial e and r
@@ -58,10 +58,10 @@ int main (int argc, char** argv) {
   // Perform PDE model
   for (std::size_t t = 0; t < num_iterations; ++t) {
     /* 
-		Write a slice from e_mesh and r_mesh.
-		Files will be headerless csv-files of a slice in the mesh.
-		Only write once per 'write_frequency' iteration.
-		*/
+    Write a slice from e_mesh and r_mesh.
+    Files will be headerless csv-files of a slice in the mesh.
+    Only write once per 'write_frequency' iteration.
+    */
     if (t % write_frequency == 0) {
       std::string e_path = "./data/e" + std::to_string(write_counter) + ".csv";
       std::string r_path = "./data/r" + std::to_string(write_counter) + ".csv";
@@ -118,12 +118,12 @@ int main (int argc, char** argv) {
     }
 
     /*
-		PDE computation by sliding stencil over inner volume 
+    PDE computation by sliding stencil over inner volume 
     Note: inner volume of padded mesh corresponds to full volume of unpadded mesh, 
-		and when calling the index function, correct width and depth must be used:
-		Variables w and d are the correct width and depth for r_mesh and e_temp.
-		Variables wp and dp are the correct width and depth for e_mesh.
-		*/
+    and when calling the index function, correct width and depth must be used:
+    Variables w and d are the correct width and depth for r_mesh and e_temp.
+    Variables wp and dp are the correct width and depth for e_mesh.
+    */
     for (std::size_t x = 1; x <= h; ++x) {
       for (std::size_t y = 1; y <= w; ++y) {
         for (std::size_t z = 1; z <= d; ++z) {
@@ -153,11 +153,11 @@ int main (int argc, char** argv) {
     for (std::size_t x = 1; x <= h; ++x) {
       for (std::size_t y = 1; y <= w; ++y) {
         for (std::size_t z = 1; z <= d; ++z) {
-          e_mesh[index(x,y,z,wp,dp)] = e_temp[index(x-1,y-1,z-1,w,d)];
+        e_mes h[index(x,y,z,wp,dp)] = e_temp[index(x-1,y-1,z-1,w,d)];
         }
       }
     }
   }
-	
-	return EXIT_SUCCESS;
+
+  return EXIT_SUCCESS;
 }
